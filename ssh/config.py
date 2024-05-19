2d54e454433243911d9f4c6db9b347a64da84ce1
DanyalTaghipor <danyqw.dt@gmail.com>
2024-05-19 14:11:19 +0330

    feat(ssh): add port option to ssh/config and refactor some texts

--- ssh/config.py
+++ ssh/config.py
@@ -1,10 +1,11 @@
 import os
 
 def set_target():
-    server = input("Enter your server address: ")
-    user = input("Enter your user: ")
-    identity_file = input(f"Enter your identity file path (default is {os.path.expanduser('~/.ssh/id_rsa')}): ") or os.path.expanduser('~/.ssh/id_rsa')
-    nickname = input("Enter your nickname: ")
+    nickname = input("Enter your nickname (Host): ")
+    server = input("Enter your server address (HostName): ")
+    user = input("Enter your user (User): ")
+    identity_file = input(f"Enter your identity file path (IdentityFile) (default is {os.path.expanduser('~/.ssh/id_rsa')}): ") or os.path.expanduser('~/.ssh/id_rsa')
+    port = input(f"Enter your port number (Port) (default is 22): ") or '22'
 
     default_path = os.path.expanduser("~/.ssh/config")
     config_path = input(f"Enter the path to your config file (default is {default_path}): ") or default_path
@@ -14,3 +15,4 @@ def set_target():
         configfile.write(f"   HostName {server}\n")
         configfile.write(f"   User {user}\n")
         configfile.write(f"   IdentityFile {identity_file}\n")
+        configfile.write(f"   Port {port}\n")
