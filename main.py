e63510b5f8909657cb83f6af078d91eeb297b26f
DanyalTaghipor <danyqw.dt@gmail.com>
2024-05-19 12:11:55 +0330

    feat(main): add main file to run app

---
+++ main.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+from db.database import init_db
+from ssh.config import set_target
+from tools import interactive_install
+
+def display_readme():
+    readme_path = os.path.join(os.path.dirname(__file__), 'README.md')
+    if os.path.exists(readme_path):
+        with open(readme_path, 'r') as file:
+            print(file.read())
+    else:
+        print("README.md file not found.")
+
+def main():
+    init_db()
+    parser = argparse.ArgumentParser(description='Linux Watch Tower', epilog='Use -h or --help for more info')
+
+    subparsers = parser.add_subparsers(dest='command')
+
+    subparsers.add_parser('set-target', help='Set a new target')
+    subparsers.add_parser('install', help='Install a tool on a target')
+
+    args = parser.parse_args()
+
+    if args.command is None:
+        display_readme()
+        return
+
+    if args.command == 'set-target':
+        set_target()
+    elif args.command == 'install':
+        interactive_install()
+
+if __name__ == "__main__":
+    main()
