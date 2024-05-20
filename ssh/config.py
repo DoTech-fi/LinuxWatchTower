b66a4cb54247ffed657bdfde1312ae52a6c61c73
DanyalTaghipor <danyqw.dt@gmail.com>
2024-05-20 17:43:27 +0330

    refactor(ssh): refactor code of set_target to force user set a value for mandatory parameters

--- ssh/config.py
+++ ssh/config.py
@@ -1,14 +1,22 @@
 import os
 
+def get_input(prompt, default=None):
+    while True:
+        value = input(f"{prompt} " + (f"(default is {default}): " if default else ": "))
+        if value or default:
+            return value or default
+        else:
+            print("This field is required. Please enter a value.")
+
 def set_target():
-    nickname = input("Enter your nickname (Host): ")
-    server = input("Enter your server address (HostName): ")
-    user = input("Enter your user (User): ")
-    identity_file = input(f"Enter your identity file path (IdentityFile) (default is {os.path.expanduser('~/.ssh/id_rsa')}): ") or os.path.expanduser('~/.ssh/id_rsa')
-    port = input(f"Enter your port number (Port) (default is 22): ") or '22'
+    nickname = get_input("Enter your nickname (Host)")
+    server = get_input("Enter your server address (HostName)")
+    user = get_input("Enter your user (User)")
+    identity_file = get_input("Enter your identity file path (IdentityFile)", os.path.expanduser('~/.ssh/id_rsa'))
+    port = get_input("Enter your port number (Port)", '22')
 
     default_path = os.path.expanduser("~/.ssh/config")
-    config_path = input(f"Enter the path to your config file (default is {default_path}): ") or default_path
+    config_path = get_input("Enter the path to your config file", default_path)
 
     with open(config_path, 'a') as configfile:
         configfile.write(f"\nHost {nickname}\n")
