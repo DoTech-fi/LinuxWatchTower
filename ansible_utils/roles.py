fdbb6b6da2343508574adc930e30137b53d0b466
DanyalTaghipor <danyqw.dt@gmail.com>
2024-05-20 12:25:03 +0330

    chore(roles): add some versions

--- ansible_utils/roles.py
+++ ansible_utils/roles.py
@@ -1,5 +1,5 @@
 from enum import Enum
 
 class Tools(Enum):
-    NGINX = {'default': 'geerlingguy.nginx', 'versions': ['latest']}
-    APACHE = {'default': 'geerlingguy.apache', 'versions': ['latest']}
+    NGINX = {'default': 'geerlingguy.nginx', 'versions': ['latest', '3.1.4', '3.1.3']}
+    APACHE = {'default': 'geerlingguy.apache', 'versions': ['latest', '3.3.0', '3.3.1']}
